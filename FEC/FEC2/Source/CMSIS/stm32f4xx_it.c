27201b468c3dc7b6437cffcae503309fe3c7c2c7
Maor Avni <LANDALABS\maora>
2016-01-31 19:18:49 +0200

    Build Fix

--- FEC/FEC2/Source/CMSIS/stm32f4xx_it.c
+++ FEC/FEC2/Source/CMSIS/stm32f4xx_it.c
@@ -67,33 +67,33 @@ void NMI_Handler(void)
 }
 
 #pragma optimize=none
+__no_init uint32_t register_r0;
+__no_init uint32_t register_r1;
+__no_init uint32_t register_r2;
+__no_init uint32_t register_r3;
+__no_init uint32_t register_r12;
+__no_init uint32_t register_lr; /* Link register. */
+__no_init uint32_t register_pc; /* Program counter. */
+__no_init uint32_t register_psr;/* Program status register. */
 void prvGetRegistersFromStack(uint32_t *pulFaultStackAddress)
 {
     /* These are volatile to try and prevent the compiler/linker optimising them
      away as the variables never actually get used.  If the debugger won't show the
      values of the variables, make them global my moving their declaration outside
      of this function. */
-    volatile uint32_t r0;
-    volatile uint32_t r1;
-    volatile uint32_t r2;
-    volatile uint32_t r3;
-    volatile uint32_t r12;
-    volatile uint32_t lr; /* Link register. */
-    volatile uint32_t pc; /* Program counter. */
-    volatile uint32_t psr;/* Program status register. */
     uint32_t mmfar;
     uint32_t hfsr;
     uint32_t cfsr;
 
-    r0 = pulFaultStackAddress[0];
-    r1 = pulFaultStackAddress[1];
-    r2 = pulFaultStackAddress[2];
-    r3 = pulFaultStackAddress[3];
+    register_r0 = pulFaultStackAddress[0];
+    register_r1 = pulFaultStackAddress[1];
+    register_r2 = pulFaultStackAddress[2];
+    register_r3 = pulFaultStackAddress[3];
 
-    r12 = pulFaultStackAddress[4];
-    lr = pulFaultStackAddress[5];
-    pc = pulFaultStackAddress[6];
-    psr = pulFaultStackAddress[7];
+    register_r12 = pulFaultStackAddress[4];
+    register_lr = pulFaultStackAddress[5];
+    register_pc = pulFaultStackAddress[6];
+    register_psr = pulFaultStackAddress[7];
 
     mmfar = SCB->MMFAR;
     hfsr = SCB->HFSR;
